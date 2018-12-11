b15d7b017ee423f2b32e2887b8e5342687493bfc
AbdSkout <abdalsk@ac.sce.ac.il>
2018-12-11 21:53:57 +0200

    background img

---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username, password;
+            StreamReader file = new StreamReader(@"C:\here\text.txt");
+            username = file.ReadLine();
+            
+
+                while (username != null)
+                {
+                    if (username == (textBox1.Text))
+                    {
+                        password = file.ReadLine();
+                        if (textBox2.Text != password)
+                        {
+                            label3.Visible = true;
+                            label3.Text = "wrong password!";
+                            break;
+                        }
+                        else if (textBox2.Text == "admin")
+                        {
+                        this.Hide();
+                        Form5 f5 = new Form5();
+                        f5.Show();
+
+                        }
+                        else
+                        {
+                            label3.Visible = true;
+                            label3.Text = "right password!";
+                        break;
+                        }
+
+
+
+
+                    }
+                    else
+                    {
+                        while (username != "***")
+                            username = file.ReadLine();
+                        username = file.ReadLine();
+                    }
+                }
+
+file.Close();
+
+            if(label3.Text== "right password!") {
+                
+                this.Hide();
+                Form4 f4 = new Form4();
+                f4.Show();
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            if (check(name))
+            {
+                this.Hide();
+                Form6 f6 = new Form6(name);
+                f6.ShowDialog();
+            }
+            else
+            {
+                label3.Text = "wrong username";
+                label3.Visible = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //StreamWriter write = new StreamWriter(@"C:\Users\abdalsk\Desktop\a",true);
+            this.Hide();
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+
+
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 f3 = new Form3();
+            f3.Show();
+
+        }
+        static bool check(string name)
+        {
+            StreamReader Read = new StreamReader(@"C:\here\text.txt");
+            string user = Read.ReadLine();
+
+
+            if (user == name)
+            {
+                Read.Close();
+                return true;
+            }
+
+            while (user != null)
+            {
+                while (user != "***")
+                {
+                    user = Read.ReadLine();
+                }
+                user = Read.ReadLine();
+
+                if (user == name)
+                {
+                    Read.Close();
+                    return true;
+
+                }
+
+            }
+            Read.Close();
+            return false;
+
+
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form6.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace WindowsFormsApp1
+{
+    public partial class Form6 : Form
+    {
+        string answer;
+        string password;
+        string username;
+        public Form6(string name)
+        {
+            InitializeComponent();
+            username = name;
+
+        }
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            StreamReader file = new StreamReader(@"C:\here\text.txt");
+            string name = file.ReadLine();
+           string temp = file.ReadLine();
+            
+            
+            if (username == name)
+            {
+                password = temp;
+                 label2.Text = file.ReadLine();
+                 answer = file.ReadLine();
+
+            }
+            else
+               while ( name != null)
+            {
+                while (name != "***")
+                    name = file.ReadLine();
+
+                name= file.ReadLine();
+                temp = file.ReadLine();
+
+                if (username == name)
+                {
+                    password = temp;
+                    label2.Text = file.ReadLine();
+                    answer = file.ReadLine();
+
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (answer == textBox1.Text)
+            {
+                label4.Text="your password is:";
+                label4.ForeColor = System.Drawing.Color.Green;
+
+                textBox2.Text = password;
+                label4.Visible = true;
+                textBox2.Visible = true;
+            }
+            else
+            {
+                label4.Visible = true;
+                label4.Text = "wrong answer!";
+                label4.ForeColor = System.Drawing.Color.Red;
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 f1 = new Form1();
+            f1.Show();
+        }
+    }
+}
