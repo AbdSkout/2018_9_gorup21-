7f6da89fcbe2e087eec29f63df5eec514da8c09d
AbdSkout <abdalsk@ac.sce.ac.il>
2019-01-04 00:47:19 +0200

    new form read feed back

--- Read feed back.cs
+++ Read feed back.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace WindowsFormsApp1
 {
     public partial class Read_feed_back : Form
     {
+        string read;
         public Read_feed_back()
         {
             InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            StreamReader file = new StreamReader("feedback.txt");
+
+            read = file.ReadLine();
+            listBox1.Items.Add("Name :" + read);
+
+            while (read != "")
+            {
+                read = file.ReadLine();
+                listBox1.Items.Add("feed is :" + read);
+                read = file.ReadLine();
+                listBox1.Items.Add("Name:" + read);
+            }
+
         }
     }
 }
