791167916db360ad250d65ca0523c1a505dd5db6
Ramiab5 <ramiab5@ac.sce.ac.il>
2019-01-05 17:25:52 +0200

    fixed tiecheck func

--- Connect4.cs
+++ Connect4.cs
@@ -96,9 +96,23 @@ namespace WindowsFormsApp1
                 {
                     Player = horizontalCheck(mat, matRow, matCol);//horizontal check function here
                 }
-                if(Player==0 && Tiecheck == 1)
+                if (Player == 0)
                 {
+                    
+                    int tie = Tiecheck(mat, matRow, matCol);
+                    if (tie == 1)
+                    {
+                        timer1.Stop();
+                        DialogResult result = MessageBox.Show("ITS A TIE !!! Do you want to continue playing?", "Connect 4", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (result == DialogResult.Yes)
+                        {
+                            this.Hide();
+                            Connect4 f10 = new Connect4();
+                            f10.ShowDialog();
 
+
+                        }
+                    }
                 }
 
 
@@ -204,9 +218,9 @@ namespace WindowsFormsApp1
             }
             if (mat[Row, Col] == player)
             {
-                if (Row == 5)
+                if (Row == 0)
                 {
-                    for(int i=0; (i<7)&&(mat[5, i]==2 || mat[5, i] == 1); i++)
+                    for(int i=0; (i<7)&&(mat[0, i]==2 || mat[0, i] == 1); i++)
                     {
                         count++;
                     }
