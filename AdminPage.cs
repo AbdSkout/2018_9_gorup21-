6732685cbac490b5613711571a90b44fd6208d71
Yaser <yaserni@ac.sce.ac.il>
2019-01-08 23:29:31 +0200

    ..

--- AdminPage.cs
+++ AdminPage.cs
@@ -20,6 +20,7 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             Connect4 s = new Connect4();
+            this.Hide();
             
         }
 
