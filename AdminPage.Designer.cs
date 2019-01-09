55dd42a7b62d9c599712ef331e5c0a36aa6270f0
Ramiab5 <ramiab5@ac.sce.ac.il>
2019-01-09 17:26:53 +0200

    ....

--- AdminPage.Designer.cs
+++ AdminPage.Designer.cs
@@ -118,7 +118,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(56, 19);
             this.button4.TabIndex = 13;
-            this.button4.Text = "Home";
+            this.button4.Text = "log out";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
