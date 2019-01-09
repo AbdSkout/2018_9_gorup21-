367a3ac220b7d42abd2fb7e5b739775e780b701f
Ramiab5 <ramiab5@ac.sce.ac.il>
2019-01-09 19:42:07 +0200

    adding result sheet form

--- trytoeditthefile.cs
+++ trytoeditthefile.cs
@@ -75,36 +75,32 @@ namespace WindowsFormsApp1
         }
         private void ReadAndShowData()
         {
-            StreamReader sr = new StreamReader("connect4.txt");
-                int i=0;
-            bool flag = false;
-                string line=sr.ReadLine(),l;
-                DataTable table = new DataTable();
-                table.Columns.Add("name");
-                table.Columns.Add("score");
-        a:
+            StreamReader cnct4 = new StreamReader("connect4.txt");
+            StreamReader snkl = new StreamReader("snake.txt");
+            int i = 0;
+            string line = cnct4.ReadLine(), l;
+            string line2 = snkl.ReadLine(), l2;
+            DataTable table = new DataTable();
+            table.Columns.Add("name");
+            table.Columns.Add("score in snakes and ladders");
+            table.Columns.Add("score in connect4");
             while (line != null)
             {
-                if (line != "***")
+                if (line != "***" && line2 != "***")
                 {
-                    l = sr.ReadLine(); i++;
-                    table.Rows.Add(line, l);
+                    l2 = snkl.ReadLine(); i++;
+                    l = cnct4.ReadLine(); i++;
+                    table.Rows.Add(line, l,l2);
                 }
-                line = sr.ReadLine();
+                line = cnct4.ReadLine();
+                i++;
+                
+                line2 = snkl.ReadLine();
                 i++;
             }
-            sr.Close();
-            if (flag == false)
-            {
-                flag = true;
-                table.Rows.Add("*******", "*******");
-                sr = new StreamReader("snake.txt");
-                line = sr.ReadLine();
-                i = 0;
-                goto a;
-            }
-            else
-                dataGridView1.DataSource = table;
+            cnct4.Close();
+            snkl.Close();
+            dataGridView1.DataSource = table;
         }
     }
 }
