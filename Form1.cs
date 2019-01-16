af1030247bf4883a3067175dfbe2dc238ba02a60
AbdSkout <abdalsk@ac.sce.ac.il>
2019-01-16 22:31:54 +0200

    ..
    ..

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string[] s;
+        public Form1()
+        {
+            InitializeComponent();
+            s = File.ReadAllLines("feedback.txt");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+    }
+}
